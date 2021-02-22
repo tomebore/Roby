44d16d8cd123313fd25c47d2d03b58e616f52d74
pricer <pricer@komatsuyuwataru-no-MacBook-Pro.local>
2021-02-22 13:36:48 +0600

    added static and media to settings and urls

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(request):
+    context = {}
+    return render(request, "base.html",context)
+
+def test(request):
+    return HttpResponse("test page ")
