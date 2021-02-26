ecd0a351f29a58546273617318c605d9a7c6b072
pricer <pricer@komatsuyuwataru-no-MacBook-Pro.local>
2021-02-26 16:32:16 +0600

    media,newsletter base css added and shop.html added

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, test
+from .views import *
 
 urlpatterns = [
     path("", home, name="home"),
+    path("shop/",shop,name="shop"),
     path("test/", test, name="test"),
 ] 
--- core/views.py
+++ core/views.py
@@ -6,5 +6,8 @@ def home(request):
     context = {}
     return render(request, "base.html",context)
 
+def shop(request):
+    context = {}
+    return render(request, "shop.html",context)
 def test(request):
     return HttpResponse("test page ")
